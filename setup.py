707ffc598b8d081051a79e7443fb2226f396d12a
haqur <haqur@HAOSIT>
2024-01-01 13:12:53 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ConfigManger',
+    version='1.0',
+    description='A useful module',
+    author='Author Name',
+    author_email='haqury@gmail.com',
+    packages=['ConfigManger'],  # same as name
+    install_requires=[],  # external packages as dependencies
+)
